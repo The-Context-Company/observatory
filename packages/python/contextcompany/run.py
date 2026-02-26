010dab1d2b240c59c4a5ca61c70fd8c7077deaa2
Rohil Agarwal <rohilvagarwal@gmail.com>
2026-02-25 22:13:20 -0800

    metadata allows straight json to be passed or takes in kwargs

--- packages/python/contextcompany/run.py
+++ packages/python/contextcompany/run.py
@@ -68,11 +68,11 @@ class Run:
         _debug("Run status set:", code, message)
         return self
 
-    def metadata(self, data: Optional[Dict[str, str]] = None, **kwargs: str) -> "Run":
+    def metadata(self, *args: Dict[str, str], **kwargs: str) -> "Run":
         if self._metadata is None:
             self._metadata = {}
-        if data is not None:
-            self._metadata.update(data)
+        if args:
+            self._metadata.update(args[0])
         self._metadata.update(kwargs)
         _debug("Run metadata:", self._metadata)
         return self
